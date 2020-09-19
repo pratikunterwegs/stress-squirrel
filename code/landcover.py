b193c32cf6ea1aef4ef1b87f709507472dec3cc9
Pratik Gupte <pratikgupte16@gmail.com>
2020-09-19 11:01:28 +0200

    check python working in vsc

---
+++ code/landcover.py
@@ -0,0 +1,13 @@
+# prelim code for earth engine landcover around duke
+
+# import libs
+import ee
+ee.Initialize()
+ee.Authenticate()
+
+# Print the elevation of Mount Everest.
+dem = ee.Image('USGS/SRTMGL1_003')
+xy = ee.Geometry.Point([86.9250, 27.9881])
+elev = dem.sample(xy, 30).first().get('elevation').getInfo()
+print('Mount Everest elevation (m):', elev)
+
