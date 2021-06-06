a4b844e40399b9ab4495f4c1149fc20bf97bff5a
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-06 17:43:36 +0200

    WIP raster saving is terrible

--- 03_ndvi_bu_cortisol.py
+++ 03_ndvi_bu_cortisol.py
@@ -3,7 +3,12 @@ import geopandas
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import box
+import rasterio
+from rasterio.plot import show
 import math
+from rasterstats import zonal_stats
+import matplotlib.pyplot as plt
+import numpy as np
 
 # read in data
 data = pd.read_excel("data/raw/hair data.xlsx")
@@ -26,10 +31,42 @@ area = 40000 # 4 hectares
 radius = math.sqrt(area / math.pi)
 
 # transform to correct UTM CRS
-data_buffer = data_sf.to_crs(epsg=32617)
+data_buffer = data_sf
+data_buffer = data_buffer.set_crs(epsg=4326)
+data_buffer = data_buffer.to_crs(epsg=32617)
 data_buffer = data_buffer.buffer(distance=radius)
+data_buffer = data_buffer.to_crs(epsg=4326)
+# save
+data_buffer.to_file(filename="data/spatial/data_cortisol_buffer.gpkg", driver="GPKG",
+                    OVERWRITE="YES")
+
 # save bounding box
 data_bbox = data_buffer.total_bounds
 data_bbox = box(*data_bbox)
 data_bbox = gpd.GeoSeries(data_bbox)
 data_bbox.to_file(filename="data/spatial/data_cortisol_bbox.shp")
+
+# rescale ndbi 0 - 1
+ndbi = rasterio.open("data/spatial/duke_built_up_cortisol.tif")
+profile = ndbi.profile
+ndbi_array = ndbi.read(1)
+ndbi_array[ndbi_array < -2] = np.nan
+
+# rescale
+ndbi_array = ndbi_array - (np.nanmin(ndbi_array)) \
+             / np.ptp(ndbi_array[np.isnan(ndbi_array) == False])
+# plot to check
+plt.imshow(ndbi_array, cmap='cividis')
+plt.colorbar()
+
+#### WORK IN PROGRESS ####
+
+#save scaled array
+ndbi_write = rasterio.open("data/spatial/duke_built_up_cortisol.tif", 'w', **profile)
+ndbi_write.write(ndbi_array.astype(rasterio.float32), 1)
+
+# get zonal stats
+ndvi_stats = zonal_stats("data/spatial/data_cortisol_buffer.gpkg",
+            "data/spatial/duke_ndvi_cortisol.tif", stats = ['mean','median'])
+ndbi_stats = zonal_stats("data/spatial/data_cortisol_buffer.gpkg",
+            "data/spatial/duke_built_up_cortisol.tif", stats = ['mean','median'])
